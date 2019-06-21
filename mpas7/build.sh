52eae8ff5db797303e2b9a69831585790699c9f5
henryo12 <homeara@uwyo.edu>
2019-06-21 17:46:04 -0600

    trying to build with paraview

--- mpas7/build.sh
+++ mpas7/build.sh
@@ -12,7 +12,8 @@ done
 module purge
 module load PrgEnv/PGI+OpenMPI/2019-04-30 
 module load pio
-module load paraview
+#module load paraview
+module load cmake/3.9.4
 
 module list
 which mpif90
@@ -23,6 +24,7 @@ ulimit -s unlimited
 module list
 export NETCDF=$NETCDF_C
 export NETCDFF=$NETCDF_F
+export PARAVIEW=/glade/work/omeara/ParaView-v5.6.1/build/
 #export NETCDF=/glade/work/cponder/SHARE/Utils/NetCDF-C/4.6.3/PGI-19.3_OpenMPI-3.1.3_PNetCDF-1.11.0_HDF5-1.10.5/
 #export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/glade/work/cponder/SHARE/Utils/NetCDF-C/4.6.3/PGI-19.3_OpenMPI-3.1.3_PNetCDF-1.11.0_HDF5-1.10.5/lib/
 
