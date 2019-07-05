89813b55d0747c0e607b2b797379d5a280c5e1e0
henryo12 <homeara@uwyo.edu>
2019-07-05 13:03:46 -0600

    linked to MPASAdaptor.so

--- mpas7/build.sh
+++ mpas7/build.sh
@@ -13,7 +13,7 @@ module purge
 module load PrgEnv/PGI+OpenMPI/2019-04-30 
 module load pio
 #module load paraview
-module load cmake/3.9.4
+#module load cmake/3.9.4
 
 module list
 which mpif90
@@ -24,13 +24,16 @@ ulimit -s unlimited
 module list
 export NETCDF=$NETCDF_C
 export NETCDFF=$NETCDF_F
-export PARAVIEW=/glade/work/omeara/ParaView-v5.6.1/build/
+export PARAVIEW=/glade/work/cponder/Applications/ParaView_pgi/build
 #export NETCDF=/glade/work/cponder/SHARE/Utils/NetCDF-C/4.6.3/PGI-19.3_OpenMPI-3.1.3_PNetCDF-1.11.0_HDF5-1.10.5/
 #export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/glade/work/cponder/SHARE/Utils/NetCDF-C/4.6.3/PGI-19.3_OpenMPI-3.1.3_PNetCDF-1.11.0_HDF5-1.10.5/lib/
+export PATH=/glade/work/omeara/Downloads/cmake-3.14.5/bin:$PATH
+#export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/glade/work/omeara/visualization/mpas7/MPAS-Model/src/paraview_catalyst/Adaptor-build/
 
 echo $LD_LIBRARY_PATH
 
 cd /gpfs/fs1/work/omeara/visualization/mpas7/MPAS-Model
+#rm -r ./src/paraview_catalyst/Adaptor-build
 
 make clean CORE=atmosphere
 make pgi CORE=atmosphere OPENACC=true PRECISION=single USE_PIO2=true DEBUG=false
